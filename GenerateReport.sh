d85743bd3ad1172e6d996ba6925440fa90f96791
Sanjay <44315700+sanjumsanthosh@users.noreply.github.com>
2024-08-08 00:58:54 +0530

    chore: Update GenerateReport.sh script
    - Update the STATIC_FOLDER path to the correct directory
    - Add an option to provide a custom folder path for file selection
    - Generate the dot file using the custom folder path
    - Update the path for running the JSON report generation script

--- GenerateReport.sh
+++ GenerateReport.sh
@@ -4,7 +4,7 @@
 
 # Constant Variable Declaration
 TARGET_REPO="/home/sanjay/Development/explore"
-STATIC_FOLDER="/home/sanjay/Development/Anki-helper-companion/static"
+STATIC_FOLDER="/home/sanjay/Development/personal/Anki-helper-companion/static"
 
 log debug "Selected Target Repo: $TARGET_REPO"
 log debug "Selected Static Folder: $STATIC_FOLDER"
@@ -18,13 +18,13 @@ read -p "Enter the github repo url: " REPO_URL_REF
 if [[ $REPO_URL_REF == *"/blob/"* ]]; then
     log debug "Repo URL is valid."
 else
-    log error "Repo URL is invalid. Exiting the program."
+    log error "Repo URL is invalid. Exiting the program. It should have /blob/ in it."
     exit 1
 fi
 
 log info "Processing url $REPO_URL_REF, Please wait..."
 
-REPO_NAME=$(echo $REPO_URL_REF | awk -F'/' '{print $4}' | awk -F'.' '{print $1}')_$(echo $REPO_URL_REF | awk -F'/' '{print $5}')
+REPO_NAME=$(echo $REPO_URL_REF | awk -F'/' '{print $5}')
 REPO_URL=$(echo $REPO_URL_REF | awk -F'/blob/' '{print $1}')
 REPO_BRANCH=$(echo $REPO_URL_REF | awk -F'/blob/' '{print $2}' | awk -F'/' '{print $1}')
 
@@ -40,28 +40,35 @@ if [ -d "$TARGET_REPO/$REPO_NAME" ]; then
     log warn "Repo already exists in the target folder. Not cloning the repo."
 else
     git clone $REPO_URL $TARGET_REPO/$REPO_NAME
+    log info "Repo cloned successfully."
 fi
 
-log info "Repo cloned successfully."
 log info "Generating the report for the repo...."
 
 current_dir=$(pwd)
 
 # Generate dot file
-cd /home/sanjay/Development/pyan 
+cd /home/sanjay/Development/personal/pyan 
 source .venv/bin/activate
-./.venv/bin/python -m pyan.main "/home/sanjay/Development/explore/mem0ai_mem0/**/*.py" --uses --no-defines --colored --grouped --annotated --dot --file=/home/sanjay/Development/Anki-helper-companion/static/mem0ai_mem0.dot
+
+# add an option to provide custom folder from which to take the files. default to all /**/ foldrs
+read -p "Do you need to provide a custom folder path to take the files from? defaults to "$TARGET_REPO/$REPO_NAME/" (y/n): " custom_folder
+customPath="$TARGET_REPO/$REPO_NAME/"
+if [ $custom_folder == "y" ]; then
+    customPath=$(find $TARGET_REPO/$REPO_NAME -type d | fzf)
+    log debug "Custom folder path selected: $customPath"
+fi
+
+# ./.venv/bin/python -m pyan.main "$TARGET_REPO/$REPO_NAME/**/*.py" --uses --no-defines --colored --grouped --annotated --dot --file=$STATIC_FOLDER/$REPO_NAME.dot
+./.venv/bin/python -m pyan.main "$customPath/**/*.py" --uses --no-defines --colored --grouped --annotated --dot --file=$STATIC_FOLDER/$REPO_NAME.dot
 cd $current_dir
 
-log debug "cd /home/sanjay/Development/pyan && /home/sanjay/Development/pyan/.venv/bin/python -m pyan.main $TARGET_REPO/$REPO_NAME/**/*.py --uses --no-defines --colored --grouped --annotated --dot --file=$STATIC_FOLDER/$REPO_NAME.dot"
-
-
+log debug "cd /home/sanjay/Development/personalpyan && /home/sanjay/Development/personalpyan/.venv/bin/python -m pyan.main $TARGET_REPO/$REPO_NAME/**/*.py --uses --no-defines --colored --grouped --annotated --dot --file=$STATIC_FOLDER/$REPO_NAME.dot"
 
 log info "Dot file generated successfully. at $STATIC_FOLDER/$REPO_NAME.dot"
 log info "Number of lines in the dot file ---->  $(wc -l $STATIC_FOLDER/$REPO_NAME.dot)"
 
 # pring how to the above command looks like 
-# echo "/home/sanjay/Development/pyan/.venv/bin/python -m pyan.main $TARGET_REPO/$REPO_NAME/**/*.py --uses --no-defines --colored --grouped --annotated --dot --file=$STATIC_FOLDER/$REPO_NAME.dot"
 
 
 # Generate JSON Report
@@ -78,7 +85,7 @@ log debug "DOT_FILE: $DOT_FILE"
 log debug "PROJECT_PATH: $PROJECT_PATH"
 log debug "GITHUB_URL: $GITHUB_URL"
 
-/home/sanjay/Development/Anki-helper-companion/processor/.venv/bin/python /home/sanjay/Development/Anki-helper-companion/processor/main.py
+/home/sanjay/Development/personal/Anki-helper-companion/processor/.venv/bin/python /home/sanjay/Development/personal/Anki-helper-companion/processor/main.py
 
 log info "JSON report generated successfully. at $STATIC_FOLDER/$REPO_NAME.json"
 
